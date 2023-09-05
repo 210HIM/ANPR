282ffde8b2e6b6f0b4afa67296d90f1107455f50
Himanshu Kalambe <110626955+210HIM@users.noreply.github.com>
2023-09-05 21:51:05 +0530

    Add files via upload

--- db_create.py
+++ db_create.py
@@ -1,23 +1,24 @@
 import mysql 
 import mysql.connector
 
-conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
-my_cursur = conn.cursor()
+def create_db():
+    conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
+    my_cursur = conn.cursor()
 
-q1="""CREATE TABLE IF NOT EXISTS `admin_data`.`admin_info` (
-  `admin_id` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `admin_name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `mobile_num` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`admin_id`));"""
-try:
-    my_cursur.execute(q1)
-    conn.commit()
-except:
-    print("not done")
-finally:
-    conn.close()
+    q1="""CREATE TABLE IF NOT EXISTS `admin_data`.`admin_info` (
+    `admin_id` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    `admin_name` VARCHAR(45) NOT NULL,
+    `email` VARCHAR(45) NOT NULL,
+    `mobile_num` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`admin_id`));"""
+    try:
+        my_cursur.execute(q1)
+        conn.commit()
+    except:
+        print("not done")
+    finally:
+        conn.close()
 
 
 
@@ -31,15 +32,37 @@ class Admnin_data:
         self.password = ''
 
 
-        conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="anpr")
-        my_cursor=conn.cursor()
-        sql='''SELECT * FROM anpr.user_data'''
-        my_cursor.execute(sql)
-        self.result=my_cursor.fetchall();
+        self.conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
+        self.my_cursor=self.conn.cursor()
+        sql='''SELECT * FROM admin_data.admin_info'''
+        self.my_cursor.execute(sql)
+        self.result=self.my_cursor.fetchall();
         #close connection here after completed programme @@@@@@
 
+        print(self.result)
+
     def Login(self):
-        print("user login Done !")
+        #user_id creation 
+        if len(self.result) > 0:
+            temp=list(self.result[-1])
+            x=temp[0]
+            a,b=x.split('_')
+            self.admin_id='admin_'+str(int(b)+1)
+        else:
+            self.admin_id = "admin_1"
+
+        print(f"{self.admin_id},{self.password},{self.name},{self.mobile_num},{self.email}")
+
+        q1 = """INSERT INTO admin_data.admin_info(admin_id,password,admin_name,email,mobile_num) VALUES(%s,%s,%s,%s,%s)"""
+        data = (self.admin_id, self.password, self.name, self.email, self.mobile_num)
+        try:
+            self.my_cursor.execute(q1,data)
+            self.conn.commit()
+        except:
+            print("not done")
+        finally:
+            self.conn.close()
+            
 
     def get_data(self):
         self.name = input("Enter Name: ")
@@ -50,26 +73,41 @@ class Admnin_data:
     def Register(self):
         self.get_data()
 
-        if len(self.name) == 0 or len(self.mobile_num) == 0 or len(self.password) == 0 or len(self.email)==0:
-            print("Data not complited !")
+        while True :
 
-        #chck name is coorect or not
-        if self.name.isalpha() == False:
-            print('Name contains only characters')
+            if len(self.name) == 0 or len(self.mobile_num) == 0 or len(self.password) == 0 or len(self.email)==0:
+                print("Data not complited !")
+                continue
 
-        #Mobile nummber chcek
-        if len(self.mobile_num) == 10 :
-            for i in range(len(self.result)):
-                if self.mobile_num in  list(self.result[i]):
-                    #show erroe masege mobile number is incurect
-                    print("Warning","Mobile no. is already registered please try again")
-                    
-        else:
-            #show erroe masege mobile number is incurect
-            print("Information","Mobile no. is invalid please try again")
-            return 2
+            #chck name is coorect or not
+            if self.name.isalpha() == False:
+                print('Name contains only characters')
+                
+
+            #Mobile nummber chcek
+            if len(self.mobile_num) == 10 :
+                if len(self.result) == 0:
+                    break
+                for i in range(len(self.result)):
+                    if self.mobile_num in  list(self.result[i]):
+                        #show erroe masege mobile number is incurect
+                        print("Warning","Mobile no. is already registered please try again")
+                        continue
+                        
+            else:
+                #show erroe masege mobile number is incurect
+                print("Information","Mobile no. is invalid please try again")
+                continue
+            
+            break
 
         self.Login()
 
 obj = Admnin_data()
 obj.Register()
+
+
+
+
+
+
