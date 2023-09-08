f3055d219e502a38fea49d709af6fba058afb94b
Himanshu Kalambe <110626955+210HIM@users.noreply.github.com>
2023-09-08 09:01:56 +0530

    Add files via upload

--- db_create.py
+++ db_create.py
@@ -2,6 +2,11 @@ import mysql
 import mysql.connector
 from tkinter import *
 from tkinter import messagebox
+from Main_window import *
+import smtplib
+from email.message import EmailMessage
+import random
+
 def create_db():
     conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
     my_cursur = conn.cursor()
@@ -17,7 +22,7 @@ def create_db():
         my_cursur.execute(q1)
         conn.commit()
     except:
-        print("not done")
+        messagebox.showerror('showerror',"Server not responding! 1")
     finally:
         conn.close()
 
@@ -25,12 +30,21 @@ def create_db():
 
 class Admnin_data:
 
-    def __init__(self):
-        self.name = ''
-        self.admin_id = ''
-        self.mobile_num = ''
-        self.email = ''
-        self.password = ''
+    def __init__(self,root):
+        self.name = None
+        self.admin_id = None
+        self.mobile_num = None
+        self.email = None
+        self.password = None
+
+        self._password = None
+        self._admin_id = None
+
+        # Login_fream root
+        self.root = root
+
+        # Mainwindo _ obj
+        self.obj = None
 
 
         self.conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
@@ -42,6 +56,16 @@ class Admnin_data:
 
     #    print(self.result)
 
+    def _result(self):
+        self.conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
+        self.my_cursor=self.conn.cursor()
+        sql='''SELECT * FROM admin_data.admin_info'''
+        self.my_cursor.execute(sql)
+        self.result=self.my_cursor.fetchall();
+        self.conn.close()
+
+        return self.result
+
     def add_admin(self):
         #user_id creation 
         if len(self.result) > 0:
@@ -52,17 +76,25 @@ class Admnin_data:
         else:
             self.admin_id = "admin_1"
 
-        print(f"{self.admin_id},{self.password},{self.name},{self.mobile_num},{self.email}")
+        print(f"{self.admin_id},{self.msg1},{self.msg4},{self.msg2},{self.msg3}")
 
         q1 = """INSERT INTO admin_data.admin_info(admin_id,password,admin_name,email,mobile_num) VALUES(%s,%s,%s,%s,%s)"""
-        data = (self.admin_id, self.password, self.name, self.email, self.mobile_num)
+        data = (self.admin_id, self.msg4, self.msg1, self.msg2, self.msg3)
         try:
             self.my_cursor.execute(q1,data)
             self.conn.commit()
+            msg='Congratulations your account has been created \n admin_id:-  '+self.admin_id
+            messagebox.showinfo('Upadet',msg)
+            sql='''SELECT * FROM admin_data.admin_info'''
+            self.my_cursor.execute(sql)
+            self.result=self.my_cursor.fetchall();
         except:
             print("not done")
+            messagebox.showerror('showerror',"server not responding ! 2")
         finally:
             self.conn.close()
+
+        self.Login()
 
     def print_msg(self,titel,msg):
         messagebox.showinfo(titel,msg)
@@ -71,11 +103,11 @@ class Admnin_data:
         # check all data is fille or not 
         if len(name) == 0 or len(mobilno) == 0 or len(password) == 0 or len(email)==0:
             messagebox.showwarning('Warning','Fill all data')
-            self.get_data()
+            self.get_data(self.root)
 
         #chck name is coorect or not
         if name.isalpha() == False:
-            self.print_msg('Information','Name contains o nly characters')
+            self.print_msg('showerror','Name contains only characters')
             return 10
 
         #Mobile nummber chcek
@@ -87,32 +119,34 @@ class Admnin_data:
                     return 12
         else:
             #show erroe masege mobile number is incurect
-            messagebox.showwarning("Information","Mobile no. is invalid please try again")
+            messagebox.showwarning("Warning","Mobile no. is invalid please try again")
             return 12
+
+        return 1
 
 
     
     def take_data(self):
-        msg1=self.admin_id.get()
-        msg2=self.email.get()
-        msg3=self.mobile_num.get()
-        msg4=self.password.get()
+        self.msg1=self.name.get()
+        self.msg2=self.email.get()
+        self.msg3=self.mobile_num.get()
+        self.msg4=self.password.get()
 
-        val=self.check_data(name=msg1,password=msg2,mobilno=msg3,email=msg4)
+        val=self.check_data(name=self.msg1,password=self.msg2,mobilno=self.msg3,email=self.msg4)
 
         if val == 1:
-            return True
+            self.add_admin()
             
         else:
             if val == 10:
-                msg1=self.name.delete(0,END)
-                self.get_data()
+                self.msg1=self.name.delete(0,END)
+                self.get_data(self.root)
             # val=check_data(name=msg1,password=msg2,mobilno=msg3,email=msg4)
 
 
             elif val == 12:
-                msg3=self.mobile_num.delete(0,END)
-                self.get_data()
+                self.msg3=self.mobile_num.delete(0,END)
+                self.get_data(self.root  )
             # val=check_data(name=msg1,password=msg2,mobilno=msg3,email=msg4)
             
            
@@ -133,8 +167,8 @@ class Admnin_data:
         #user_id
         Name=Label(Datafreame2,text="Name:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=2,column=0,pady=15,sticky=W)
 
-        self.admin_id=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
-        self.admin_id.grid(row=2,column=1,padx=5)
+        self.name=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
+        self.name.grid(row=2,column=1,padx=5)
 
         #mobil_number
         Mobil_no=Label(Datafreame2,text="Mobil No:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=3,column=0,pady=15,sticky=W)
@@ -149,7 +183,7 @@ class Admnin_data:
         self.email.grid(row=4,column=1,padx=5)
 
         #pass_word
-        self.password=Label(Datafreame2,text="Password:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=5,column=0,pady=15,sticky=W)
+        password=Label(Datafreame2,text="Password:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=5,column=0,pady=15,sticky=W)
 
         self.password=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
         self.password.grid(row=5,column=1,padx=5)
@@ -157,16 +191,14 @@ class Admnin_data:
         Login_butt=Button(Datafreame2,text='Register',width=15,command=self.take_data)
         Login_butt.grid(row=7,column=1,pady=5)
 
-        BACK=Button(Datafreame2,text='Login',width=15,command=self.Login_take_chek)
+        BACK=Button(Datafreame2,text='Login',width=15,command=self.Login)
         BACK.grid(row=9,column=1,padx=5)
 
-    def Register(self,root):
-        Info = self.get_data(root)
-        if Info == True: 
-            self.add_admin()
+    def Register(self):
+         self.get_data(self.root)
 
-    def Login(self,root):
-        Datafreame2=Frame(root,background='#4caedb')
+    def Login(self):
+        Datafreame2=Frame(self.root,background='#4caedb')
         Datafreame2.place(x=490,y=30,width=300,height=350)  
         #title
         input_label=Label(Datafreame2,text='Login',fg='white',bg='#013764',font=("times new roman",13,'bold'),relief=RAISED,padx=10)
@@ -192,15 +224,150 @@ class Admnin_data:
         BACK=Button(Datafreame2,text='Register',width=15,command=self.Register)
         BACK.grid(row=7,column=1,pady=15)
 
+        Forget_pass=Button(Datafreame2,text='Forget Password',width=20,command=self.reset_password)
+        Forget_pass.grid(row=7,column=1,pady=15)
+
     def Login_take_chek(self):
-        self.admin_id=self.admin_id.get()
-        self.password=self.password.get()
+        admin_id=self.admin_id.get()
+        password=self.password.get()
+        # new updated data from _result function 
+        self.result = self._result() 
         for item in self.result:
-            if self.admin_id in item and self.password in item:
+            if admin_id in item and password in item:
                 print("sucefully login !")
+                messagebox.showinfo('Upadet',"sucefully login !")
+                self.root.destroy()
+                self.obj = Main_windows(admin_id=admin_id)
+                break
+        else: 
+            print("not login !")
+            messagebox.showwarning('showwarning',"User_id,Password not present !")
+
+    def reset_password(self):
+        Datafreame2=Frame(self.root,background='#4caedb')
+        Datafreame2.place(x=490,y=30,width=300,height=350)
+
+        #title
+        input_label=Label(Datafreame2,text='Reset Password',fg='white',bg='#013764',font=("times new roman",13,'bold'),relief=RAISED)
+        input_label.grid(padx=0,pady=0)
+
+        temp_lable=Label(Datafreame2,bg='#4caedb')
+        temp_lable.grid(row=1,column=0,padx=10)
+
+        
+        #email
+        emil=Label(Datafreame2,text="Email -Id:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=2,column=0,pady=15,sticky=W)
+
+        self.email=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
+        self.email.grid(row=2,column=1,padx=5)
 
 
-            
+        Login_butt=Button(Datafreame2,text='Send Otp',width=15,command=self.Otp_Eamil)
+        Login_butt.grid(row=3,column=1,pady=5)
+
+        
+    def Otp_Eamil(self):
+        self.OTP=random.randint(10000,99999)
+        print(self.OTP)
+        addreas = self.email.get()
+        print(addreas)
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login('rytcareer14@gmail.com','nqmmdsxvgfzyguxw')
+        masg="""Dear user \nYour one time password:"""+str(self.OTP )+"""Please note that the one time password is valid for only one session.]nIf you did not request a one time password please connect with us immediately at rytcareer14@gmail.com.\nRegards,Automatic License/Number Plate Recognition (ANPR)"""
+
+        email = EmailMessage()
+        #email['From'] =
+        email['To'] =addreas
+        email['Subject'] = 'OTP'
+        email.set_content(masg)
+        server.send_message(email)
+
+        self.temp()
+        
+    def temp(self):
+        Datafreame2=Frame(self.root,background='#4caedb')
+        Datafreame2.place(x=490,y=30,width=300,height=350)
+
+        #title
+        input_label=Label(Datafreame2,text='OTP Check',fg='white',bg='#013764',font=("times new roman",13,'bold'),relief=RAISED)
+        input_label.grid(padx=0,pady=0)
+
+        temp_lable=Label(Datafreame2,bg='#4caedb')
+        temp_lable.grid(row=1,column=0,padx=10)
+
+        
+        #email
+        otp=Label(Datafreame2,text="Enter OTP:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=2,column=0,pady=15,sticky=W)
+
+        self.user_otp=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
+        self.user_otp.grid(row=2,column=1,padx=5)
+
+
+        Login_butt=Button(Datafreame2,text='Set Password',width=15,command=self.chek_OTP)
+        Login_butt.grid(row=3,column=1,pady=5)
+
+    def chek_OTP(self):
+        otp = self.user_otp.get()
+        print(f"{otp}=={self.OTP}")
+
+        if  str(otp) == str(self.OTP):
+            Datafreame2=Frame(self.root,background='#4caedb')
+            Datafreame2.place(x=490,y=30,width=300,height=350)
+
+            #title
+            input_label=Label(Datafreame2,text='Set Password',fg='white',bg='#013764',font=("times new roman",13,'bold'),relief=RAISED)
+            input_label.grid(padx=0,pady=0)
+
+            temp_lable=Label(Datafreame2,bg='#4caedb')
+            temp_lable.grid(row=1,column=0,padx=10)
+
+            #admin_id
+            admin=Label(Datafreame2,text="Admin ID:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=2,column=0,pady=15,sticky=W)
+
+            self.admin_id=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
+            self.admin_id.grid(row=2,column=1,padx=5)
+
+            #pass_word
+            password=Label(Datafreame2,text="Password:",font=("arial",10,"bold"),padx=2,pady=4,fg='white',bg='#0071a7').grid(row=3,column=0,pady=15,sticky=W)
+
+            self.password=Entry(Datafreame2,font=("arial",10,"bold"),bg='#81c3e5',width=22,fg='black')
+            self.password.grid(row=3,column=1,padx=5)
+        
+
+            Login_butt=Button(Datafreame2,text='Login',width=15,command=self.set_new_Password)
+            Login_butt.grid(row=4,column=1,padx=5)
+        
+        else:
+            self.temp()
+            print("otp is wrong")
+
+    def set_new_Password(self):
+        Pass = str(self.password.get())
+        admin_id = str(self.admin_id.get())
+        self.conn=mysql.connector.connect(host="localhost",username="root",password="rakesh",database="admin_data")
+        self.my_cursor=self.conn.cursor()
+        sql = '''UPDATE `admin_data`.`admin_info` SET `password` = %s WHERE (`admin_id` = %s);'''
+        data = (Pass , admin_id)
+        print(admin_id)
+        try:
+            self.my_cursor.execute(sql,data)
+            self.conn.commit()
+            print("Password chnage and save")
+            self.Login()
+        except:
+            print("New password is not change and save ")
+        finally:
+            self.conn.close()
+
+
+
+                
+
+
+
+
+                
 
 
 
